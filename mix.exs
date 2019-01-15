280ec9d6fe968cff61424cbfe5e28d9360491f5f
Simone Cottini <simone.cottini@prima.it>
2019-01-15 14:14:52 +0100

    elixir version at least 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Csv.Schema.MixProject do
     [
       app: :csv_schema,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       elixirc_paths: ["lib"],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
