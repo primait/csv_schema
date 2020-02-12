d32fe91be00e1d4a63d0b27538bf6f523fcacc36
Simone Cottini <simone.cottini@prima.it>
2020-02-12 17:24:56 +0100

    0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Csv.Schema.MixProject do
   def project do
     [
       app: :csv_schema,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.6",
       elixirc_paths: elixir_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
